adc892f6d476c8983f09ab7ff3b8f46dfb5b3ae2
gyenney <gregory.yenney@gmail.com>
2015-03-13 17:17:38 -0700

    Create Rover.h

---
+++ Robotics/Lesson-14---Multiple-Rover-Objects/Rover/Rover.h
@@ -0,0 +1,49 @@
+//
+#ifndef Rover_h
+#define Rover_h
+
+#include <Arduino.h>
+#include <Servo.h>
+
+#define LEFT_TURN 0
+#define RIGHT_TURN 1
+
+class Rover
+{
+ private:
+ 
+ int _servoLeftStop;
+ int _servoRightStop;
+ Servo _servoLeft;
+ Servo _servoRight;
+ int _debug;
+
+ public:
+ Rover();
+ void init ( int servoLeftPin,
+             int servoLeftStop,
+             int servoRightPin,
+             int servoRightStop );
+
+ void go(int speed);
+
+ void go(int speed,int time);
+
+ void turn(int direction, int turnSpeed);
+
+ void turn(int direction,int speed, int time);
+
+ void stop();
+
+ void stop(int time);
+
+ void debugOn();
+
+ void debugOff();
+
+ void debugPrintln(char * str);
+
+ void debugPrint(char * str);
+};
+#endif
+       
